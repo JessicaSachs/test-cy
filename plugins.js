37ddfcfcd501ef39c4db8122f03125e15c461005
Jessica Sachs <jess@jessicasachs.io>
2021-04-02 04:08:14 -0400

    test

---
+++ plugins.js
@@ -0,0 +1,8 @@
+const { startDevServer } = require('@cypress/webpack-dev-server')
+
+module.exports = (on, config) => {
+    on('dev-server:start', (options) => {
+        return startDevServer({ options, webpackConfig: {} })
+    })
+    return config
+}
